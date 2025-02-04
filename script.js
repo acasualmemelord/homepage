edcdb30a7c1139361c15a2b1a85bfb79edc54a2d
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2025-02-04 12:54:10 -0800

    haha

--- script.js
+++
@@ -1,2 +0,0 @@
-// Access the name from the environment variable created in the workflow
-document.getElementById('name-placeholder').textContent = window.nameFromEnv;window.nameFromEnv = 'Robert Tran';
