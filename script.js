b2b6becd62d29f3c6fdd7ecf433785fdc7690ed9
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2025-02-04 21:36:40 +0000

    Deploying to gh-pages from @ acasualmemelord/homepage@bf8330b14c6b15fc1fcd8ab789a1a9d28641c378 🚀

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const namePlaceholder = document.getElementById('name-placeholder');
-
-// Fetch the name from the environment variable injected by the workflow
 const concealedName = window.nameFromEnv;
 
 namePlaceholder.textContent = concealedName;
