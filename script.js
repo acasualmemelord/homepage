3bec9946a3906e4c1ceea5f1182eee9af958e55b
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2025-02-04 22:03:50 +0000

    Deploying to gh-pages from @ acasualmemelord/homepage@6d0afc1e9998160fded576f2bafff746ffc7ba99 🚀

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
   const namePlaceholder = document.getElementById('name-placeholder');
+  const getName = window.getName; // Get the function from the injected code
 
-  // No fetch, no placeholders.  Generate content dynamically.
-  const concealedName = window.nameFromEnv; // Get from env variable
-
-  if (concealedName) { // Check if the variable exists (important for local testing)
-      namePlaceholder.textContent = concealedName;
+  if (getName) { // Check if the function exists (important for local testing)
+    namePlaceholder.textContent = getName(); // Call the function to get the name
   } else {
-      namePlaceholder.textContent = "Name not available."; // Or some other default message
+    namePlaceholder.textContent = "Name not available."; // Default message
   }
-});window.nameFromEnv = 'Robert Tran';
+});window.getName = function() { return 'Robert Tran'; };
