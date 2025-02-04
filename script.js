6ec2f90af1749818ccc9d46039cac9c88b80dae0
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2025-02-04 21:50:39 +0000

    Deploying to gh-pages from @ acasualmemelord/homepage@2ec801e0fbaf21eb49bd41ae7314c9ab1f4b503d 🚀

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const namePlaceholder = document.getElementById('name-placeholder');
-  const concealedName = window.nameFromEnv;  // <-- This will be replaced
+  const concealedName = window.nameFromEnv;
 
   namePlaceholder.textContent = concealedName;
-});
-
-// Placeholder for replacement:  Robert Tran
+});window.nameFromEnv = 'Robert Tran';
