991a41d9b5714f03093990a76a69bdd73d7862b9
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2025-02-04 20:44:11 +0000

    Deploying to gh-pages from @ acasualmemelord/homepage@412b71f6ec736648c4e5a83ce8648ba5f7c5183a 🚀

---
+++ script.js
@@ -0,0 +1,2 @@
+// Access the name from the environment variable created in the workflow
+document.getElementById('name-placeholder').textContent = window.nameFromEnv;window.nameFromEnv = 'Robert Tran';
