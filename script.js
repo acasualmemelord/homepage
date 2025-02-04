bbd3089c31f296e7f914be426ae8af08ae54c7d9
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2025-02-04 20:59:17 +0000

    Deploying to gh-pages from @ acasualmemelord/homepage@312aca4473478a871621edd293259271afae5f89 🚀

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const namePlaceholder = document.getElementById('name-placeholder');
-const concealedName = namePlaceholder.dataset.name; // Get the name from the data attribute
+const concealedName = namePlaceholder.dataset.name;
 
-namePlaceholder.textContent = concealedName === "__NAME__" ? "Loading..." : concealedName; // Handle placeholder case
+namePlaceholder.textContent = concealedName; // Set the textContent
