2ec801e0fbaf21eb49bd41ae7314c9ab1f4b503d
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2025-02-04 13:50:21 -0800

    Update jekyll-gh-pages.yml

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const namePlaceholder = document.getElementById('name-placeholder');
-  const concealedName = window.nameFromEnv;  // <-- This will be replaced
+  const concealedName = window.nameFromEnv;
 
   namePlaceholder.textContent = concealedName;
 });
-
-// Placeholder for replacement:  REPLACE_ME_WITH_NAME
